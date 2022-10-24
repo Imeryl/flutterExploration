b505c9bee6fd144fb7e9204cccb10a30e7dbc65f
Cara <cara@churchsuite.com>
2022-10-24 16:26:41 +0100

    Chp 54
    Getters & else-if

--- quiz_app/lib/result.dart
+++ quiz_app/lib/result.dart
@@ -5,11 +5,34 @@ class Result extends StatelessWidget {
 
   Result(this.score);
 
+  String get resultPhrase {
+    var resultText = 'Answers submitted. ';
+
+    if (score <= 8) {
+      resultText += 'Love you!';
+    } else if (score <= 12) {
+      resultText += 'Pretty good.';
+    } else if (score <= 16) {
+      resultText += 'Eh.';
+    } else {
+      resultText += 'Well this is awkward...';
+    }
+
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text('Answers submitted, you scored: ' + score.toString()),
+        child: Text(
+          resultPhrase,
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
       ),
     );
   }
