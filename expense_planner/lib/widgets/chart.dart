8369295ac659d582aa8aa206f66abe4282584df9
Cara <cara@churchsuite.com>
2022-10-25 14:15:37 +0100

    Chp 100 Looping through lists

---
+++ expense_planner/lib/widgets/chart.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart({required this.recentTransactions});
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+      double totalSum = 0;
+      for (final transaction in recentTransactions) {
+        if (DateFormat.yMd(transaction.date) == DateFormat.yMd(weekDay)) {
+          totalSum += transaction.amount;
+        }
+      }
+
+      print(weekDay);
+      print(totalSum);
+
+      return {
+        'day': DateFormat.E(weekDay),
+        'amount': totalSum,
+      };
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
