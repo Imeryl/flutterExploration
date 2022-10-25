193060983fc90f284afc66b980ec3309d06ced1f
Cara <cara@churchsuite.com>
2022-10-25 12:12:23 +0100

    Chp 94 Showing bottom modal

--- expense_planner/lib/main.dart
+++ expense_planner/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'widgets/app_body.dart';
+import 'models/transaction.dart';
+import 'widgets/new_transaction.dart';
+import 'widgets/transaction_list.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,26 +16,82 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: '1',
+      title: 'Shoes',
+      amount: 35.00,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: '1',
+      title: 'Take out',
+      amount: 16.46,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addTransaction(String title, double amount) {
+    setState(() {
+      _userTransactions.add(
+        Transaction(
+          id: DateTime.now().toString(),
+          title: title,
+          amount: amount,
+          date: DateTime.now(),
+        ),
+      );
+    });
+  }
+
+  void showAddTransaction(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return NewTransaction(_addTransaction);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         actions: <Widget>[
           IconButton(
-            onPressed: null,
+            onPressed: () => showAddTransaction(context),
             icon: Icon(Icons.add),
           ),
         ],
         backgroundColor: Colors.purple,
         title: Text('Flutter App'),
       ),
-      body: AppBody(),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Card(
+                child: Text('Chart'),
+                elevation: 5,
+              ),
+              TransactionList(
+                transactions: _userTransactions,
+              )
+            ],
+          ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton.extended(
         backgroundColor: Colors.purple,
         icon: Icon(Icons.add),
         label: Text('Add Transaction'),
-        onPressed: null,
+        onPressed: () => showAddTransaction(context),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
