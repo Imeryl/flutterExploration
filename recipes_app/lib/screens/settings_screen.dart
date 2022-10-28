8d7d022982ec8c783be959e098ba8d814b59b5c1
Cara <cara@churchsuite.com>
2022-10-28 13:15:47 +0100

    Chp 180
    Adding settings switches

--- recipes_app/lib/screens/settings_screen.dart
+++ recipes_app/lib/screens/settings_screen.dart
@@ -2,9 +2,28 @@ import 'package:flutter/material.dart';
 
 import '../widgets/main_drawer.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   static const routeName = '/settings';
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _glutenFree = false;
+  bool _lactoseFree = false;
+  bool _vegan = false;
+  bool _vegetarian = false;
+
+  SwitchListTile _buildSwitch(
+      bool value, String text, Function(bool) updateValue) {
+    return SwitchListTile(
+      onChanged: updateValue,
+      title: Text(text),
+      value: value,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,8 +32,25 @@ class SettingsScreen extends StatelessWidget {
         title: Text('Settings'),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text('Settings'),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            child: Text('Adjust your meal selection.'),
+            padding: EdgeInsets.all(20),
+          ),
+          Expanded(
+            child: ListView(children: <Widget>[
+              _buildSwitch(_glutenFree, 'Gluten free',
+                  (newValue) => setState(() => _glutenFree = newValue)),
+              _buildSwitch(_lactoseFree, 'Lactose free',
+                  (newValue) => setState(() => _lactoseFree = newValue)),
+              _buildSwitch(_vegan, 'Vegan',
+                  (newValue) => setState(() => _vegan = newValue)),
+              _buildSwitch(_vegetarian, 'Vegetarian',
+                  (newValue) => setState(() => _vegetarian = newValue)),
+            ]),
+          ),
+        ],
       ),
     );
   }
