8fb080110f2ec26f2d77813d27a72ba4f4157883
Cara <cara@churchsuite.com>
2022-10-31 14:23:06 +0000

    Chp 203
    Working with multiple Providers

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'providers/cart.dart';
 import 'providers/products.dart';
 import 'screens/product_screen.dart';
 import 'screens/products_screen.dart';
@@ -10,8 +11,11 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => Cart()),
+        ChangeNotifierProvider(create: (_) => Products()),
+      ],
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
