37ac72723375ea19e046d959c2f1cfafd87f0e79
Cara <cara@churchsuite.com>
2022-11-03 15:43:04 +0000

    Chp 259
    Future builders and code improvements

--- shop_app/lib/screens/orders_screen.dart
+++ shop_app/lib/screens/orders_screen.dart
@@ -5,49 +5,36 @@ import '../providers/orders.dart';
 import '../widgets/app_drawer.dart';
 import '../widgets/order_item.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
   const OrdersScreen({super.key});
 
   @override
-  State<OrdersScreen> createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isInit = true;
-  var _isLoading = false;
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    if (_isInit) {
-      setState(() => _isLoading = true);
-      Provider.of<Orders>(
-        context,
-        listen: false,
-      ).fetchOrders().then((value) => setState(() => _isLoading = false));
-    }
-    _isInit = false;
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final orderProvider = Provider.of<Orders>(context);
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Your orders'),
       ),
       drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemBuilder: (context, index) =>
-                  OrderItem(orderProvider.orders[index]),
-              itemCount: orderProvider.orders.length,
-            ),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(
+          context,
+          listen: false,
+        ).fetchOrders(),
+        builder: ((context, snapshot) {
+          return snapshot.connectionState == ConnectionState.waiting
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Consumer<Orders>(
+                  builder: (context, orderProvider, child) => ListView.builder(
+                    itemBuilder: (context, index) =>
+                        OrderItem(orderProvider.orders[index]),
+                    itemCount: orderProvider.orders.length,
+                  ),
+                );
+        }),
+      ),
     );
   }
 }
