d173ed29aaf8648856414d940ed8027a02102cce
Cara <cara@churchsuite.com>
2022-11-03 14:54:14 +0000

    Chp 254
    Updating isFavourite to update the DB

--- shop_app/lib/providers/product.dart
+++ shop_app/lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +20,22 @@ class Product with ChangeNotifier {
     this.isFavourite = false,
   });
 
-  void toggleFavourite() {
+  Future<void> toggleFavourite() {
+    final oldStatus = isFavourite;
     isFavourite = !isFavourite;
     notifyListeners();
+    final url = Uri.parse(
+        'https://flutter-udemy-cead0-default-rtdb.europe-west1.firebasedatabase.app/products/${id}.json');
+    return http
+        .patch(
+      url,
+      body: json.encode({
+        'isFavourite': isFavourite,
+      }),
+    )
+        .catchError((error) {
+      isFavourite = oldStatus;
+      throw error;
+    });
   }
 }
