bc3b8a255d21bf7db7adae31c00d9e3faf7d7c5f
Cara <cara@churchsuite.com>
2022-11-14 11:51:28 +0000

    Chp 298
    Using image picker

--- native_features/lib/widgets/image_input.dart
+++ native_features/lib/widgets/image_input.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -11,6 +12,14 @@ class ImageInput extends StatefulWidget {
 
 class _ImageInputState extends State<ImageInput> {
   File? _storedImage;
+
+  Future<void> _takePicture() async {
+    final picker = ImagePicker();
+    final imageFile = await picker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +45,7 @@ class _ImageInputState extends State<ImageInput> {
         ),
         Expanded(
           child: TextButton(
-            onPressed: () {},
+            onPressed: _takePicture,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
