8fca0de6a6d1e83d11dd72e48264ed9939abca2b
Cara <cara@churchsuite.com>
2022-11-15 20:20:32 +0000

    Started auth form

--- chat_app/lib/widgets/auth_form.dart
+++ chat_app/lib/widgets/auth_form.dart
@@ -8,22 +8,52 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  void trySubmit() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           TextFormField(
             decoration: const InputDecoration(labelText: 'Email'),
             keyboardType: TextInputType.emailAddress,
+            validator: (value) {
+              if (value == null) return 'Please enter a value';
+              if (value.isEmpty || !value.contains('@')) {
+                return 'Please enter a valid email address';
+              }
+              return null;
+            },
           ),
           TextFormField(
             decoration: const InputDecoration(labelText: 'Username'),
+            validator: (value) {
+              if (value == null) return 'Please enter a value';
+              if (value.isEmpty) {
+                return 'Please enter a valid username';
+              }
+              return null;
+            },
           ),
           TextFormField(
-            decoration: const InputDecoration(labelText: 'Email'),
+            decoration: const InputDecoration(labelText: 'Password'),
             obscureText: true,
+            validator: (value) {
+              if (value == null) return 'Please enter a value';
+              if (value.isEmpty || value.length < 7) {
+                return 'Password must be at least 7 characters';
+              }
+              return null;
+            },
           ),
           const SizedBox(
             height: 10,
